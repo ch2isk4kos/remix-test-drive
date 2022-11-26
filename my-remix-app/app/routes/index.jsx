51d9face7a2d4fe34443e0bc2b7cef375a927eb8
ch2isk4kos <ch2isk4kos@gmail.com>
2022-11-26 15:59:57 -0500

    add navbar

--- my-remix-app/app/routes/index.jsx
+++ my-remix-app/app/routes/index.jsx
@@ -1,32 +1,26 @@
+import { Link } from "@remix-run/react";
+
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+      <nav aria-label="Main navigation" className="remix-app__header-nav">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/movies">Movies Search Demo</Link>
+          </li>
+          <li>
+            <Link to="/facets">Facet Search Demo</Link>
+          </li>
+          <li>
+            <a href="https://github.com/mongodb-developer/atlas-data-api-remix">
+              GitHub
+            </a>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 }
