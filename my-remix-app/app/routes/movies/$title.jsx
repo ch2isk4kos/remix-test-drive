8b270f6a2e9722ba780e30e2d6b2b5edba440404
ch2isk4kos <ch2isk4kos@gmail.com>
2022-11-26 16:54:14 -0500

    add movie details component to render api data

--- my-remix-app/app/routes/movies/$title.jsx
+++ my-remix-app/app/routes/movies/$title.jsx
@@ -36,3 +36,52 @@ export let loader = async ({ params }) => {
     image: movie.poster,
   };
 };
+
+export default function MovieDetails() {
+  let movie = useLoaderData();
+
+  return (
+    <div>
+      <h1>{movie.title}</h1>
+      {movie.plot}
+      <br></br>
+      <div styles="padding: 25% 0;" class="tooltip">
+        <li>Year</li>
+        <Link
+          class="tooltiptext"
+          to={"../movies?filter=" + JSON.stringify({ year: movie.year })}
+        >
+          {movie.year}
+        </Link>
+      </div>
+      <br />
+      <div styles="padding: 25% 0;" class="tooltip">
+        <li>Genres</li>
+        <Link
+          class="tooltiptext"
+          to={"../movies?filter=" + JSON.stringify({ genres: movie.genres })}
+        >
+          {movie.genres.map((genre) => {
+            return genre + " | ";
+          })}
+        </Link>
+      </div>
+      <br />
+      <div styles="padding: 25% 0;" class="tooltip">
+        <li>Directors</li>
+        <Link
+          class="tooltiptext"
+          to={
+            "../movies?filter=" + JSON.stringify({ directors: movie.directors })
+          }
+        >
+          {movie.directors.map((director) => {
+            return director + " | ";
+          })}
+        </Link>
+      </div>
+      <br></br>
+      <img src={movie.image} alt={""}></img>
+    </div>
+  );
+}
